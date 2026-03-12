675cd2ebd9552fc62ccea528ce6e2aac4a39d946
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2026-03-12 12:35:02 +0700

    btn Update

--- Form1.cs
+++ Form1.cs
@@ -76,7 +76,20 @@ namespace PraktikumADO
             }
         }
 
-       
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi();
+                conn.Open();
+                string query = "UPDATE Mahasiswa SET Alamat='Yogyakarta' WHERE NIM='2310100001'";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+                conn.Close();
+
+            }
+            
 
 
 
